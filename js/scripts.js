bb4a5dacea085235c60ba03b13e5c2b2147420aa
Kantimahanty Rohit <61279841+Kay-Rohit@users.noreply.github.com>
2020-05-19 22:11:47 +0530

    NPM scripts

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({
+        interval: 2000
+    });
+    $("#carouselButton").click(function(){
+        if($('#carouselButton').children('span').hasClass('fa-pause')){
+            $("#mycarousel").carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if($('#carouselButton').children('span').hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('toggle');
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('toggle');
+    });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+    $("#close1").click(function(){
+        $("#loginModal").modal('hide');
+    });
+    $("#close2").click(function(){
+        $("#reserveModal").modal('hide');
+    });
+    $(".close").click(function(){
+        $("#reserveModal").modal('hide');
+        $("#loginModal").modal('hide');
+    });
+});
